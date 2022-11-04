fce879895bc8698de0e8fdb413600f3e7bb6ebfb
lhh <liuhonghao@whu.edu.cn>
2022-11-04 16:30:06 +0800

    fix index stats

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 from mana.models import AttachedUser
+
+import datetime
 
 # Create your models here.
 
@@ -225,3 +228,39 @@ class Document(models.Model):
 
     def __str__(self):
         return ''
+    
+# 访问网站的 ip 地址、端点和次数
+class UserIP(models.Model):
+    ip = models.CharField(verbose_name='IP 地址', max_length=30)
+    end_point = models.CharField(verbose_name='访问端点', default='/', max_length=30)
+    count = models.IntegerField(verbose_name='访问次数', default=0)
+    
+    class Meta:
+        verbose_name = '访问用户信息'
+        verbose_name_plural = verbose_name
+        
+    def __str__(self):
+        return self.ip
+        
+# 网站总访问次数
+class VisitNumber(models.Model):
+    count = models.IntegerField(verbose_name='网站访问总次数', default=0) # 网站访问总次数
+    
+    class Meta:
+        verbose_name = '网站访问总次数'
+        verbose_name_plural = verbose_name
+        
+    def __str__(self):
+        return str(self.count)
+ 
+# 单日访问量统计
+class DayNumber(models.Model):
+    day = models.DateField(verbose_name='日期', default=timezone.now)
+    count = models.IntegerField(verbose_name='网站访问次数', default=0) # 网站访问总次数
+    
+    class Meta:
+        verbose_name = '网站日访问量统计'
+        verbose_name_plural = verbose_name
+        
+    def __str__(self):
+        return str(self.day)
